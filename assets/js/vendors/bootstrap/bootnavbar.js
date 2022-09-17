c60f9439d2a6cd8c87a7d42faa75ebb9fc68fd7a
Hussien Kamal <Hussien.Kamal@linkdev.com>
2022-09-17 08:05:07 +0200

    finish nav UI for desktop and start in responsive

--- assets/js/vendors/bootstrap/bootnavbar.js
+++ assets/js/vendors/bootstrap/bootnavbar.js
@@ -13,12 +13,6 @@ function bootnavbar(options) {
       .getElementsByClassName("dropdown");
 
     Array.prototype.forEach.call(dropdowns, (item) => {
-      //add animation
-      if (bnOptions.animation) {
-        const element = item.querySelector(".dropdown-menu");
-        element.classList.add("animate__animated");
-        element.classList.add(bnOptions.animateIn);
-      }
 
       //hover effects
       item.addEventListener("mouseover", function () {
